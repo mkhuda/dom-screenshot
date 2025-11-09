ec75cf0302bd7573a0f73e918d0f0af2459e450b
Lek Huda <mhmmd.huda@gmail.com>
2025-11-09 18:31:35 +0700

    feat: migrate to TypeScript with modern build tooling

---
+++ rollup.config.mjs
@@ -0,0 +1,35 @@
+import typescript from '@rollup/plugin-typescript';
+import terser from '@rollup/plugin-terser';
+import copy from 'rollup-plugin-copy';
+
+export default {
+  input: 'src/dom-screenshot.ts',
+  output: [
+    {
+      file: 'dist/dom-screenshot.min.js',
+      format: 'iife',
+      name: 'domtoimage',
+      sourcemap: true,
+      plugins: [
+        terser({
+          compress: {
+            drop_console: false,
+          },
+          format: {
+            comments: false,
+          },
+        }),
+      ],
+    },
+    {
+      file: 'dist/dom-screenshot.esm.js',
+      format: 'esm',
+      sourcemap: true,
+    },
+  ],
+  plugins: [
+    typescript({
+      tsconfig: './tsconfig.json',
+    }),
+  ],
+};
